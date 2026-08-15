6ae8195ea411311b110aefe3bce8d775bbd8dba8
Adharsh Sheelam <144654013+Adharsh-Sheelam@users.noreply.github.com>
2026-08-15 17:47:42 +0530

    Change assertion from NotNull to Null for result

--- tests/UnitTests/MediatorHandlers/OrdersTests/GetOrderDetails.cs
+++ tests/UnitTests/MediatorHandlers/OrdersTests/GetOrderDetails.cs
@@ -34,6 +34,6 @@ public class GetOrderDetails
 
         var result = await handler.Handle(request, CancellationToken.None);
 
-        Assert.NotNull(result);
+        Assert.Null(result);
     }
 }
